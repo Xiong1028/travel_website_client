590b5c4d9a0bedc4f944ae8cbd5e437228fc7ef9
xiong1028 <lixiong1124@gmail.com>
2019-01-25 14:52:34 -0500

    build project directory

---
+++ src/sharedComponents/header/index.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import './header.css';
+import {Row, Col, Grid} from "react-bootstrap";
+
+
+class Header extends Component{
+    render(){
+        return(
+            <Grid className='header  container-fluid'>
+                <Row className='show-grid'>
+                    <Col md={12}>Header Component goes here</Col>
+                </Row>
+            </Grid>
+        )
+    }
+}
+
+
+export default Header;
---
+++ src/store/reducer.js
@@ -0,0 +1,8 @@
+import {combineReducers} from 'redux-immutable';
+
+//guarantee the state is immutable
+const reducer = combineReducers({
+
+})
+
+export default reducer;
