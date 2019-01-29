35ca10e1cfe4f84b64d6928b9b3ca2fca5cf464e
xiong1028 <lixiong1124@gmail.com>
2019-01-28 20:04:08 -0500

    modify index.js

--- src/sharedComponents/header/index.js
+++ src/sharedComponents/header/index.js
@@ -3,11 +3,10 @@ import {connect} from 'react-redux';
 import './header.css';
 import {Row, Col, Grid} from "react-bootstrap";
 
-
 class Header extends Component{
     render(){
         return(
-            <Grid className='header  container-fluid'>
+            <Grid className='header container-fluid'>
                 <Row className='show-grid'>
                     <Col md={12}>Header Component goes here</Col>
                 </Row>
