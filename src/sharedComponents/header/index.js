5daf4410863ab87c6ea4195c5c2b3317f2ec1999
xiong1028 <lixiong1124@gmail.com>
2019-01-29 00:00:03 -0500

    create a header

--- src/sharedComponents/header/index.js
+++ src/sharedComponents/header/index.js
@@ -1,19 +1,66 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
-import './header.css';
-import {Row, Col, Grid} from "react-bootstrap";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import "./header.css";
+import {
+  Navbar,
+  Nav,
+  NavItem,
+  NavDropdown,
+  MenuItem,
+  FormGroup,
+  FormControl,
+  Button
+} from "react-bootstrap";
 
-class Header extends Component{
-    render(){
-        return(
-            <Grid className='header container-fluid'>
-                <Row className='show-grid'>
-                    <Col md={12}>Header Component goes here</Col>
-                </Row>
-            </Grid>
-        )
-    }
+class Header extends Component {
+  render() {
+    return (
+      <Navbar fluid fixdtop collapseOnSelect inverse>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <a href="#">Travel</a>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav>
+            <NavItem>Home</NavItem>
+            <NavItem>Link</NavItem>
+            <NavDropdown eventKey={3} title="Forum" id="basic-nav-dropdown">
+              <MenuItem eventKey={3.1}> Action </MenuItem>
+              <MenuItem eventKey={3.2}> Another action </MenuItem>
+              <MenuItem eventKey={3.3}> Something else here </MenuItem>
+              <MenuItem divider />
+              <MenuItem eventKey={3.3}> Separated link </MenuItem>
+            </NavDropdown>
+          </Nav>
+          <Nav pullRight>
+            <Navbar.Form pullLeft>
+              <FormGroup>
+                <FormControl type="text" placeholder="Search" />
+              </FormGroup>{" "}
+              <Button type="submit">Search</Button>
+            </Navbar.Form>
+            <NavItem eventKey={1} href="#">
+              Link Right
+            </NavItem>
+            <NavItem eventKey={2} href="#">
+              Link Right
+            </NavItem>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    );
+  }
 }
 
+const mapStateToProps = state => {
+  return {};
+};
 
-export default Header;
+const mapDispatchToProps = dispatch => {};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Header);
