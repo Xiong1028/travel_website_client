49fb51848d1ad4ae866b6f3acba5547ad429ed42
Hui Sun <j.sun71839@gmail.com>
2019-02-01 16:44:13 -0500

    photo slide refine

--- src/bodyComponents/home/index.jsx
+++ src/bodyComponents/home/index.jsx
@@ -1,55 +1,22 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { Carousel } from 'react-bootstrap';
+import  PhotoSlide  from './components/photoSlide';
 
 
 
 class Home extends Component {
   render() {
     return (
-      <Carousel>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="https://pixabay.com/photo-1499171/"
-            alt="First slide"
-          />
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="https://pixabay.com/photo-1365387/"
-            alt="Third slide"
-          />
-
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="https://pixabay.com/photo-569315/"
-            alt="Third slide"
-          />
-
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
+      <PhotoSlide />
     ); 
   }
 }
-const mapStateToProps = state => ({});
+const mapStateToProps = state => {
+  return {};
 
-const mapDispatchToProps = dispatch => ({});
+};
+
+const mapDispatchToProps = dispatch => {};
 
 export default connect(
   mapStateToProps,
