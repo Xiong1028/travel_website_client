56be5941eb1bb05837bd70a62caf9830d0ac9418
Hui Sun <j.sun71839@gmail.com>
2019-03-09 14:10:56 -0500

    fix tabs and profile component

--- src/publicComponents/setting/components/profile.jsx
+++ src/publicComponents/setting/components/profile.jsx
@@ -1,10 +1,118 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { Upload, Icon, message } from 'antd';
+import { Slider, Upload, Icon, message, Modal, Col } from 'antd';
+import AvatarEditor from 'react-avatar-editor';
+import { Button } from 'react-bootstrap';
+import Divider from '@material-ui/core/Divider';
 import { actionCreators } from '../store';
 
 import '../setting.css';
 
+class Profile extends Component {
+
+    state = {
+        file: null,
+        scale: 1 
+    };
+    
+    
+
+    handleUpload = (file) => {
+        console.log(file);
+        this.setState({ file: file });
+    }
+
+    onChange = (value) => {
+        this.setState({ scale: value });
+    }
+
+    onClickSave = () => {
+        if (this.editor) {
+            // This returns a HTMLCanvasElement, it can be made into a data URL or a blob,
+            // drawn on another canvas, or added to the DOM.
+            const canvas = this.editor.getImage().toDataURL();
+            console.log(canvas);
+
+            let imageURL;
+            fetch(canvas).then(res => {
+                res.blob();
+                console.log(res.blob());
+            })
+                         .then(blob => (imageURL = window.URL.createObjectURL(blob)));
+            
+        }
+    }
+
+    setEditorRef = (editor) => this.editor = editor;
+
+    render() {
+        const { file, scale } = this.state;
+
+        const uploadButton = (
+            <div>
+                <Icon type='plus' />
+                <div className="ant-upload-text">Upload</div>
+            </div>
+        );
+
+        return (
+            <div>
+                <h4 style={{ marginBottom: '5%' }}>Update Profile</h4>
+                <Divider style={{ width: '150%', marginBottom: '5%' }} />
+                <div className="clearfix">
+                    <Upload
+                        listType="picture-card"
+                        showUploadList={false}
+                        action={this.handleUpload}
+                    >
+                        {uploadButton}
+                    </Upload>
+                </div>
+                <AvatarEditor
+                    ref={this.setEditorRef}
+                    image={file}
+                    width={200}
+                    height={200}
+                    border={50}
+                    borderRadius={100}
+                    color={[255, 255, 255, 0.4]} // RGBA
+                    scale={scale}
+                    rotate={0}
+                />
+                <Col span={12}>
+                    <Slider
+                        min={1}
+                        max={2}
+                        step={0.1}
+                        onChange={this.onChange}
+                    />
+                </Col>
+                <Button variant="outline-primary" onClick={this.onClickSave}>Save</Button>
+
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+
+    };
+};
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Profile);
+
+// Use Antd Upload Component to upload avatar
+/*
 function beforeUpload(file) {
     const isType = (file.type === 'image/jpeg') || (file.type === 'image/png');
     if (!isType) {
@@ -18,36 +126,38 @@ function beforeUpload(file) {
 }
 
 
-
-
-class Avatar extends Component {
+class Profile extends Component {
     state = {
         previewVisible: false,
         previewImage: '',
-        file: null,
-        imgUrl: ''
+        file: [{
+            uid: '-1',
+            name: 'xxx.png',
+            status: 'done',
+            url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png'
+        }],
+        imgUrl: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png'
     };
-    
 
-    
     handlePreview = (file) => {
+        console.log(file);
+        console.log(file.thumbUrl);
         this.setState({
             previewImage: file.url || file.thumbUrl,
             previewVisible: true
         });
-    }   
+    }
+
+    handleCancel = () => this.setState({ previewVisible: false });
 
     handleChange = (info) => {
-        console.log(info.file.originFileObj);
         this.setState({
             file: info.file.originFileObj
         });
-        
+
         this.getBase64(info.file.originFileObj, imageUrl => this.setState({
             imageUrl: imageUrl
         }));
-
-        
     }
 
     getBase64(img, callback) {
@@ -57,6 +167,7 @@ class Avatar extends Component {
     }
 
     render() {
+        const { previewVisible, previewImage } = this.state;
         const { handleUpload } = this.props;
 
         const uploadButton = (
@@ -66,18 +177,25 @@ class Avatar extends Component {
             </div>
         );
 
+
+
         return (
-            <Upload
-                name="avatar"
-                className="avatar-uploader"
-                listType="picture-card"
-                action={(file) => handleUpload(file)}
-                beforeUpload={beforeUpload}
-                onPreview={this.handlePreview}
-                onChange={this.handleChange}
-            >
-               {uploadButton}
-            </Upload>
+            <div className="clearfix">
+                <Upload
+                    name="avatar"
+                    className="avatar-uploader"
+                    listType="picture-card"
+                    action={(file) => handleUpload(file)}
+                    beforeUpload={beforeUpload}
+                    onPreview={this.handlePreview}
+                    onChange={this.handleChange}
+                >
+                {uploadButton}
+                </Upload>
+                <Modal visible={previewVisible} footer={null} onCancel={this.handleCancel}>
+                    <img alt="example" style={{ width: '100%' }} src={previewImage} />
+                </Modal>
+            </div>
         );
     }
 }
@@ -91,10 +209,18 @@ const mapStateToProps = state => {
 const mapDispatchToProps = dispatch => {
     return {
         handleUpload(file) {
-            console.log(file);
             let formData = new FormData();
             formData.append('avatar', file);
-            console.log(formData);
+
+            // retrieve formData
+
+            let formKeys    = formData.keys();
+            var formEntries = formData.entries();
+            do {
+                console.log(formEntries.next().value);
+            } while (!formKeys.next().done)
+
+
 //            dispatch(actionCreators.handleUploadAction(formData));
         }
     };
@@ -103,4 +229,5 @@ const mapDispatchToProps = dispatch => {
 export default connect(
     mapStateToProps,
     mapDispatchToProps
-)(Avatar);
+)(Profile);
+*/
--- src/publicComponents/setting/index.jsx
+++ src/publicComponents/setting/index.jsx
@@ -1,31 +1,39 @@
 import React, {Component} from "react";
 import { Tab, ListGroup, Row, Col } from 'react-bootstrap';
+import {Icon} from 'antd';
 
-import Avatar from './components/profile';
+import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
+
+import Profile from './components/profile';
+
+// react-bootstrap list group
 class Setting extends Component {
     render() {
-        return (
-            <Tab.Container id="list-group-tabs-example" defaultActiveKey="#profile">
-                <Row>
-                    <Col sm={4}>
-                        <ListGroup>
-                            <ListGroup.Item action href="#profile">Profile</ListGroup.Item>
-                            <ListGroup.Item action href="#password">Password</ListGroup.Item>
-                        </ListGroup>
-                    </Col>
-                    <Col sm={8}>
-                        <Tab.Content>
-                            <Tab.Pane eventKey="#profile">
-                                <Avatar />
-                            </Tab.Pane>
-                            <Tab.Pane eventKey="#password">
-                            
-                            </Tab.Pane>
-                        </Tab.Content>
-                    </Col>
-                </Row>
-            </Tab.Container>            
+        return ( 
+            <div className="container">
+                <Tab.Container id="list-group-tabs-example" defaultActiveKey="#profile">
+                    <Row>
+                        <Col sm={4}>
+                            <ListGroup style={{fontSize: "17px"}}>
+                                <ListGroup.Item action href="#profile"><Icon type="user" style={{float: 'left', margin:'2.5%'}} />Update Profile</ListGroup.Item>
+                                <ListGroup.Item action href="#password"><Icon type="unlock" style={{float: 'left', margin:'2.5%'}} />Change Password</ListGroup.Item>
+                            </ListGroup>
+                        </Col>
+                        <Col sm={8}>
+                            <Tab.Content>
+                                <Tab.Pane eventKey="#profile">
+                                    <Profile />
+                                </Tab.Pane>
+                                <Tab.Pane eventKey="#password">
+                                
+                                </Tab.Pane>
+                            </Tab.Content>
+                        </Col>
+                    </Row>
+                </Tab.Container>   
+            </div>      
+               
         );
     }
 }
