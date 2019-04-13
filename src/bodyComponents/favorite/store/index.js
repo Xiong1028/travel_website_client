ebda98f0ef7fba8b358f074fd4baa866dd807464
Hui Sun <j.sun71839@gmail.com>
2019-04-13 19:31:28 -0400

    finish my favorite articles page

---
+++ src/bodyComponents/favorite/store/index.js
@@ -0,0 +1,6 @@
+import favoriteReducer from './favoriteReducer';
+import * as actionCreators from './actionCreators';
+import * as authorActionCreators from '../../author/store/actionCreators';
+import * as constants from './constants';
+
+export {favoriteReducer, actionCreators, authorActionCreators, constants};
